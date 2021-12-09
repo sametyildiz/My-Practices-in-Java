e47a816499b046286af418d69f14f649440fdce0
Samet YILDIZ <sametyildiz03@gmail.com>
2021-12-09 23:21:48 +0300

    making solution to problem28 more efective

--- src/my_practice/leetcode/easyproblems/Problem28.java
+++ src/my_practice/leetcode/easyproblems/Problem28.java
@@ -15,23 +15,17 @@ public class Problem28 {
      * For the purpose of this problem, we will return 0 when needle is an empty string. This is consistent to C's strstr() and Java's indexOf().
      */
 
-    public int strStr(String haystack, String needle) {//Time Limit Exceeded
+    public int strStr(String haystack, String needle) {
         if(needle.isEmpty())
             return 0;
 
-        int index = 0 , needleIndex = 0;
-        while (index <= haystack.length() - needle.length()){
-            if(needleIndex == needle.length())
+        int index = 0 , hL = haystack.length() , nL = needle.length();
+        while (index <= hL - nL){
+            if(haystack.substring(index , index + nL).equals(needle))
                 return index;
-            if(haystack.charAt(index + needleIndex) == needle.charAt(needleIndex))
-                needleIndex++;
-            else {
-                needleIndex = 0;
+            else
                 index++;
-            }
-
         }
         return -1;
      }
-
 }
