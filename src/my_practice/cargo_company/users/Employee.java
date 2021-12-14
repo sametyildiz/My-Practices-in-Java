ab096fb20641a7e80f7af628b7542b381b0c92a2
Samet YILDIZ <sametyildiz03@gmail.com>
2021-12-14 12:52:01 +0300

    editing folders

--- src/my_practice/cargo_company/users/Employee.java
+++
@@ -1,9 +0,0 @@
-package my_practice.cargo_company.users;
-
-public class Employee extends Users {
-
-    public Employee(String name, String surname) {
-        super(name, surname, UsersType.employee);
-    }
-
-}
--- src/my_practice/cargo_company/users/Transporter.java
+++
@@ -1,7 +0,0 @@
-package my_practice.cargo_company.users;
-
-public class Transporter extends Users{
-    public Transporter(String name, String surname) {
-        super(name, surname, UsersType.transportationPersonnel);
-    }
-}
